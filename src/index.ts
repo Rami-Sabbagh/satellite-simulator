e01e8ca4c3dbea464d64d7aea887c502317aba2d
Rami Sabbagh <rami.sab07@gmail.com>
2022-05-07 01:32:04 +0300

    Enable physically correct lights

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z = 2;
 
 const renderer = new THREE.WebGLRenderer();
+renderer.physicallyCorrectLights = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
