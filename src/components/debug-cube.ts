6ed9ecc18f0254956fc3c08f1353033b3ded114d
Rami Sabbagh <rami.sab07@gmail.com>
2022-05-07 01:33:24 +0300

    Correct a spelling typo.

--- src/components/debug-cube.ts
+++ src/components/debug-cube.ts
@@ -3,12 +3,12 @@ import * as THREE from 'three';
 // The instances can be shared, because all debug cubes have the same shape, material, etc...
 
 const geometry = new THREE.BoxGeometry();
-const meterial = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 });
 
-const mesh = new THREE.Mesh(geometry, meterial);
+const mesh = new THREE.Mesh(geometry, material);
 
 // And the object 3d will apply all the wished transformations per each instance,
 // such as scaling, position, rotation etc...
