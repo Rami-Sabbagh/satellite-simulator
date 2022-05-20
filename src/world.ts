f5462e6f7c7095dfcd6fa453c1caa0f83b142d36
Rami Sabbagh <rami.sab07@gmail.com>
2022-05-20 11:46:24 +0300

    Add a simulated-space to the 3D world

--- src/world.ts
+++ src/world.ts
@@ -1,18 +1,26 @@
 import * as THREE from 'three';
 
-import DebugSphere from 'components/debug-sphere';
+import SimulatedSpace from 'components/simulated-space';
+import Planet from 'components/planet';
+import Satellite from 'components/satellite';
 
 export default class World extends THREE.Scene {
     protected clock = new THREE.Clock();
+    protected simulatedSpace = new SimulatedSpace(1e-6);
 
     constructor() {
         super();
+        this.add(this.simulatedSpace.scene);
 
-        this.add(new DebugSphere());
+        this.simulatedSpace.add(new Planet());
+        this.simulatedSpace.add(Satellite.spawn(
+            new THREE.Vector3(1.3, 0, 0), //position
+            new THREE.Vector3(0, 0, 800), //velocity
+        ));
     }
 
     update() {
         const dt = this.clock.getDelta();
-        
+        this.simulatedSpace.run(dt/1000);
     }
 }
