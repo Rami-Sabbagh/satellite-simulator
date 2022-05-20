2759532903c810e454fdb63b96c4088bfde50e46
Rami Sabbagh <rami.sab07@gmail.com>
2022-05-20 20:41:57 +0300

    Allow tweaking the orbit parameters using lil gui

--- src/world.ts
+++ src/world.ts
@@ -1,3 +1,4 @@
+import GUI from 'lil-gui';
 import * as THREE from 'three';
 
 import SimulatedSpace from 'components/simulated-space';
@@ -10,39 +11,51 @@ export default class World extends THREE.Scene {
     protected clock = new THREE.Clock();
     protected simulatedSpace = new SimulatedSpace(1e-6);
 
+    protected orbitalElements: OrbitalElements = {
+        trueAnomaly: 0,
+        argumentOfPeriapsis: 0,
+
+        eccentricity: 0,
+        semiMajorAxis: 1,
+
+        inclination: 0,
+        longitudeOfAscendingNode: 0,
+    };
+
+    protected satellite: Satellite;
+
     constructor() {
         super();
-        this.add(this.simulatedSpace.scene);
 
         this.add(new AxesHelper(.2));
+        this.add(this.simulatedSpace.scene);
 
         this.simulatedSpace.add(new Planet());
 
-        const orbitalElements: OrbitalElements = {
-            trueAnomaly: 0,
-            argumentOfPeriapsis: Math.PI * 0,
+        const {position, velocity} = calculateStateVectors(this.orbitalElements, 1 + 1_000_000);
+        this.satellite = Satellite.spawn(position, velocity)
+        this.simulatedSpace.add(this.satellite);
 
-            eccentricity: 0.2,
-            semiMajorAxis: 1,
+        this.showGUI();
+    }
 
-            inclination: Math.PI * 0,
-            longitudeOfAscendingNode: Math.PI * 0,
-        };
+    showGUI() {
+        const gui = new GUI({title: 'Satellite Orbit Elements', width: 500});
 
-        console.info('Initial Orbital Elements:');
-        console.table(orbitalElements);
+        gui.add( this.orbitalElements, 'eccentricity').name('Eccentricity').min(0).max(1);
+        gui.add( this.orbitalElements, 'semiMajorAxis').name('Semi-Major Axis').min(0).max(5);
+        gui.add( this.orbitalElements, 'inclination').name('Inclination').min(0).max(Math.PI);
+        gui.add( this.orbitalElements, 'trueAnomaly').name('True Anomaly').min(0).max(Math.PI);
+        gui.add( this.orbitalElements, 'argumentOfPeriapsis').name('Argument of Periapsis').min(0).max(Math.PI);
+        gui.add( this.orbitalElements, 'longitudeOfAscendingNode').name('Longitude of Ascending Node').min(0).max(Math.PI);
 
-        const stateVectors = calculateStateVectors(orbitalElements, 1 + 1_000_000);
-
-        console.info('Initial state vectors:');
-        console.table(stateVectors);
-
-        this.simulatedSpace.add(Satellite.spawn(
-            stateVectors.position,
-            stateVectors.velocity,
-            // new THREE.Vector3(1.3, 0, 0), //position
-            // new THREE.Vector3(0, 0, -800), //velocity
-        ));
+        gui.add({
+            apply: () => {
+                const {position, velocity} = calculateStateVectors(this.orbitalElements, 1 + 1_000_000);
+                this.satellite.position.copy(position);
+                this.satellite.velocity.copy(velocity);
+            }
+        }, 'apply').name('Apply State Vectors');
     }
 
     update() {
