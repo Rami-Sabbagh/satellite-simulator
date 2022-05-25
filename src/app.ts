4f4c0cd60b33931cacde02761d9523215a3c07af
Rami Sabbagh <rami.sab07@gmail.com>
2022-05-25 18:14:47 +0300

    Access the planet's color by instance instead of a module import

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import World from 'world';
-import { planetProperties } from 'components/planet';
 
 /**
  * THREE.js Application.
