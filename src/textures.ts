e59ad13a7ab2c9d28e2db1f358555b73abd9c8c3
Rami Sabbagh <rami.sab07@gmail.com>
2022-05-28 13:55:05 +0300

    Add `Texture` suffix to the planet texture names

--- src/textures.ts
+++ src/textures.ts
@@ -14,14 +14,14 @@ export interface TexturePack {
 
 const loader = new THREE.TextureLoader();
 
-export const earth: Partial<TexturePack> = {
+export const earthTexture: Partial<TexturePack> = {
     colorMap: loader.load(earthColorMap),
 };
 
-export const jupiter: Partial<TexturePack> = {
+export const jupiterTexture: Partial<TexturePack> = {
     colorMap: loader.load(jupiterColorMap),
 };
 
-export const mars: Partial<TexturePack> = {
+export const marsTexture: Partial<TexturePack> = {
     colorMap: loader.load(marsColorMap),
 };
