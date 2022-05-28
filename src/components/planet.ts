b25a24ac3f9037c8274f365b050a61844b2fae3a
Rami Sabbagh <rami.sab07@gmail.com>
2022-05-28 13:51:37 +0300

    Rewrite `setTexture`

--- src/components/planet.ts
+++ src/components/planet.ts
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 
+import { TexturePack } from 'textures';
+
 import SimulatedObject from 'components/simulated-object';
+
 import { Body, BodyType, ExertsForce } from 'physics/body';
 import { GRAVITATION_CONSTANT } from 'physics/constants';
 
@@ -38,15 +41,11 @@ export default class Planet extends SimulatedObject implements ExertsForce {
         // the force is now calculated properly and ready to use.
     }
 
-    setTexture(texturePack:any) {
-        this.material = new THREE.MeshStandardMaterial({
-            map: texturePack.colorMap.image ? texturePack.colorMap : undefined,
-            bumpMap: texturePack.colorMap.image ? texturePack.bumpMap : undefined,
-            aoMap: texturePack.colorMap.image ? texturePack.aoMap : undefined,
-            emissiveMap: texturePack.colorMap.image ? texturePack.emissiveMap : undefined,
-            metalnessMap: texturePack.colorMap.image ? texturePack.metalnessMap : undefined,
-
-        });
-        this.mesh.material =  this.material;
+    setTexture(pack: Partial<TexturePack>) {
+        this.material.map = pack.colorMap ?? null;
+        this.material.bumpMap = pack.bumpMap ?? null;
+        this.material.aoMap = pack.aoMap ?? null;
+        this.material.emissiveMap = pack.emissiveMap ?? null;
+        this.material.metalnessMap = pack.metalnessMap ?? null;
     }
 }
