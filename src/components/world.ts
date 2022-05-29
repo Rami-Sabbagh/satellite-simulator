819c7463a90e76519f0c15b49006fdceb955d0d0
Yaman Qassas <yaman102011@gmail.com>
2022-05-29 23:01:21 +0300

    Add sun to scene

--- src/components/world.ts
+++ src/components/world.ts
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import SimulatedSpace from 'components/simulated-space';
 import Planet from 'components/planet';
 import { AxesHelper } from 'three';
+import Sun from './sun';
 export default class World extends THREE.Scene {
     protected clock = new THREE.Clock();
     protected simulatedSpace = new SimulatedSpace(1e-6);
@@ -17,8 +18,13 @@ export default class World extends THREE.Scene {
         this.add(light);
 
         this.add(new AxesHelper(.2));
+
+        const sun = new Sun();
+        sun.position.set(1, 0, 200);
+        this.add(sun);
+
         this.simulatedSpace.addTo(this);
-        
+
         this.simulatedSpace.add(this.planet);
     }
 
