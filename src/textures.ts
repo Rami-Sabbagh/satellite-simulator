2df138f237e5570537b84d0e66476b730efac659
Edward alkarra <edward@big-bang.ae>
2022-05-29 23:07:08 +0300

    added texture map images, nested the texture maps into proper folder structure, added bumpMap property to the TexturePack interface

--- src/textures.ts
+++ src/textures.ts
@@ -1,13 +1,17 @@
 import * as THREE from 'three';
 
-import earthColorMap from 'assets/earth.jpg';
-import jupiterColorMap from 'assets/jupiter.jpg';
-import marsColorMap from 'assets/mars.jpg';
+import earthColorMap from 'assets/textures/earth-texture-maps/earth-color.jpg';
+import earthBumpMap from 'assets/textures/earth-texture-maps/earth-bump.jpg';
+import jupiterColorMap from 'assets/textures/jupiter-texture-maps/jupiter-color.jpg';
+import marsColorMap from 'assets/textures/mars-texture-maps/mars-color.jpg';
+import marsBumpMap from 'assets/textures/mars-texture-maps/mars-bump.jpg';
+
 import sunColorMap from 'assets/sun.jpg';
 
 export interface TexturePack {
     colorMap: THREE.Texture,
     bumpMap: THREE.Texture,
+    bumpScale: number,
     aoMap: THREE.Texture,
     emissiveMap: THREE.Texture,
     metalnessMap: THREE.Texture,
@@ -17,6 +21,8 @@ const loader = new THREE.TextureLoader();
 
 export const earthTexture: Partial<TexturePack> = {
     colorMap: loader.load(earthColorMap),
+    bumpMap: loader.load(earthBumpMap),
+    bumpScale: 0.2,
 };
 
 export const jupiterTexture: Partial<TexturePack> = {
@@ -25,6 +31,8 @@ export const jupiterTexture: Partial<TexturePack> = {
 
 export const marsTexture: Partial<TexturePack> = {
     colorMap: loader.load(marsColorMap),
+    bumpMap: loader.load(marsBumpMap),
+    bumpScale: 0.2,
 };
 
 export const sunTexture: Partial<TexturePack> = {
