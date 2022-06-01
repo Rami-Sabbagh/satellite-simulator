868c400dfe448fa8acab361888015c80e37f9293
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-01 18:56:56 +0300

    Allow the modification of the object's mass

--- src/components/simulated-object.ts
+++ src/components/simulated-object.ts
@@ -4,13 +4,17 @@ import { Body, BodyType } from '../physics/body';
 export default class SimulatedObject extends THREE.Object3D implements Body {
     get physicsType() { return this._physicsType; }
     get mass() { return this._mass; }
+    set mass(value: number) {
+        this.velocity.multiplyScalar(Math.sqrt(this._mass / value));
+        this._mass = value;
+    }
     
     velocity = new THREE.Vector3();
     appliedForces = new THREE.Vector3();
 
     constructor(
         private readonly _physicsType: BodyType,
-        private readonly _mass: number,
+        private _mass: number,
     ) {
         super();
     }
