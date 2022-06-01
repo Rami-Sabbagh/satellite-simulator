35adb539523e7662d669e4ddc37d80310e667aa8
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-01 17:34:54 +0300

    Remove the lunch vector from the world

--- src/components/world.ts
+++ src/components/world.ts
@@ -4,20 +4,17 @@ import SimulatedSpace from 'components/simulated-space';
 import Planet from 'components/planet';
 
 import Sun from './sun';
-import StateVectorsArrow from "./state-vectors-arrow";
 
 export default class World extends THREE.Scene {
     protected clock = new THREE.Clock();
     protected simulatedSpace = new SimulatedSpace(1e-6);
 
     readonly planet = new Planet();
-    readonly launchVector = new StateVectorsArrow();
 
     constructor() {
         super();
 
         this.add(new THREE.AxesHelper(.2));
-        this.add(this.launchVector);
         this.add(new THREE.AmbientLight(0xffffff, 0.5));
 
         this.add(new Sun());
