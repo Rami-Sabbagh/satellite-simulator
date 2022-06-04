09644a56cf2f0ec3303d41a410ada54f524f1b91
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-04 14:17:15 +0300

    Develop the satellites spawner

--- src/components/ghost-satellite.ts
+++ src/components/ghost-satellite.ts
@@ -1,14 +1,12 @@
 import * as THREE from 'three';
 import { StateVectors } from 'physics/kepler-math';
 
-const geometry = new THREE.SphereGeometry(7, 4, 2);
+const geometry = new THREE.SphereGeometry(7e5, 4, 2);
 const material = new THREE.MeshBasicMaterial({
-    opacity: 0.5,
+    opacity: .5,
     color: 0x00ffff,
     wireframe: true,
 });
-
-const mesh = new THREE.Mesh(geometry, material);
 
 const tempVector = new THREE.Vector3();
 
@@ -16,18 +14,22 @@ export default class GhostSatellite extends THREE.Object3D {
     protected positionArrow = new THREE.ArrowHelper(undefined, undefined, undefined, 0xff0000, .05, .01);
     protected velocityArrow = new THREE.ArrowHelper(undefined, undefined, undefined, 0xffff00, .1, .05);
 
+    protected body = new THREE.Mesh(geometry, material);
+
     constructor() {
         super();
 
         this.add(this.positionArrow);
         this.add(this.velocityArrow);
-        this.add(mesh);
+        this.add(this.body);
 
         this.visible = false; // invisible by default.
         // this.positionArrow.visible = false;
     }
 
     set state({ position, velocity }: StateVectors) {
+        this.body.position.copy(position);
+
         this.velocityArrow.setLength(velocity.length());
         this.velocityArrow.setDirection(tempVector.copy(velocity).normalize());
         this.velocityArrow.position.copy(position);
--- src/interface/spawn-interface.ts
+++ src/interface/spawn-interface.ts
@@ -1,30 +1,107 @@
 import GUI from 'lil-gui';
 import type Application from 'app';
-import { ManualElements } from 'physics/kepler-math';
+
+import { Euler, Vector3 } from 'three';
+
+import { EARTH_RADIUS } from 'physics/constants';
+import { OrbitalElements, StateVectors } from 'physics/kepler-math';
+
+const TO_DEGREE = 180 / Math.PI;
+const TO_RADIAN = Math.PI / 180;
+
+const tempEuler = new Euler();
+
+function degreeView(record: Record<string, number>): typeof record {
+    const descriptors: PropertyDescriptorMap = {};
+
+    for (const key in record) {
+        descriptors[key] = {
+            get() { return record[key] * TO_DEGREE; },
+            set(value: number) { record[key] = value * TO_RADIAN; }
+        };
+    }
+
+    return Object.defineProperties({}, descriptors);
+}
 
 export default class SpawnInterface {
     protected folder = this.gui.addFolder('Satellite Creation');
 
-    protected spawnElements: ManualElements = {
+    options = {
+        preview: this.app.world.ghost.visible,
+    };
+
+    manualFolder = this.folder.addFolder('State Vectors');
+
+    manual = {
+        velocity: 2e6,
+        height: EARTH_RADIUS * 2,
+
+        longitude: 0,
+        latitude: 0,
         inclination: 0,
+    };
+
+    manualView = degreeView(this.manual);
+
+    state: StateVectors = {
+        position: new Vector3(this.manual.height, 0, 0),
+        velocity: new Vector3(),
+    };
+
+    kepler: OrbitalElements = {
+        eccentricity: 0,
+        semiMajorAxis: this.manual.height,
+        inclination: 0,
+        longitudeOfAscendingNode: 0,
+        argumentOfPeriapsis: 0,
         trueAnomaly: 0,
-        semiMajorAxis: .7,
-        velocity: 1,
     };
 
     constructor(protected readonly gui: GUI, protected app: Application) {
         this.folder.open(false); // closed by default.
+        this.manualFolder.open(false); // closed by default.
 
-        const minRadius = this.app.world.planet.radius;
-        const maxRadius = this.app.world.planet.radius * 2;
+        this.folder.add(this.options, 'preview').name('Preview');
 
-        this.folder.add(this.spawnElements, 'inclination').name('Inclination').min(0).max(Math.PI);
-        this.folder.add(this.spawnElements, 'trueAnomaly').name('True Anomaly').min(0).max(Math.PI * 2);
-        this.folder.add(this.spawnElements, 'semiMajorAxis').name('Semi-Major Axis').min(minRadius).max(maxRadius);
-        this.folder.add(this.spawnElements, 'velocity').name('Velocity').min(0).max(5);
+        this.manualFolder.add(this.manual, 'velocity').name('Velocity').min(2e6).max(2e7);
+        this.manualFolder.add(this.manual, 'height').name('Height').min(EARTH_RADIUS * 1.25).max(EARTH_RADIUS * 10);
+
+        this.manualFolder.add(this.manualView, 'longitude').name('Longitude').min(-180).max(180);
+        this.manualFolder.add(this.manualView, 'latitude').name('Latitude').min(-180).max(180);
+        this.manualFolder.add(this.manualView, 'inclination').name('Inclination').min(-180).max(180);
+
+        this.manualFolder.onChange(this.applyManual.bind(this));
+
+        this.folder.onChange(this.apply.bind(this));
+        this.apply();
     }
 
     hotReplaceApplication(app: Application) {
         this.app = app;
+    }
+
+    protected apply() {
+        this.app.world.ghost.visible = this.options.preview;
+        this.app.world.ghost.state = this.state;
+    }
+
+    protected applyManual() {
+        tempEuler.set(
+            this.manual.inclination,
+            this.manual.longitude,
+            this.manual.latitude,
+            'YZX'
+        );
+
+        this.state.position
+            .set(this.manual.height, 0, 0)
+            .applyEuler(tempEuler);
+
+        this.state.velocity
+            .set(0, 0, -this.manual.velocity)
+            .applyEuler(tempEuler);
+        
+        this.app.world.ghost.state = this.state;
     }
 }
