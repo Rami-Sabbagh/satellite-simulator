1c0ebbfe321d6d2194302a000ff2081300b37de0
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-04 16:59:34 +0300

    Add an interface for setting simulation options

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -3,23 +3,25 @@ import GUI from 'lil-gui';
 
 import type Application from 'app';
 
+import SimulationInterface from './simulation-interface';
 import CameraInterface from './camera-interface';
 import PlanetInterface from './planet-interface';
 import SpawnInterface from './spawn-interface';
 import SunInterface from './sun-interface';
-
 export default class Interface {
     readonly gui = new GUI({
         title: 'Satellites Simulator VI: Deluxe Edition',
         width: 500,
     });
 
+    protected readonly simulation: SimulationInterface;
     protected readonly sun: SunInterface;
     protected readonly planet: PlanetInterface;
     protected readonly spawn: SpawnInterface;
     protected readonly camera: CameraInterface;
 
     constructor(protected app: Application) {
+        this.simulation = new SimulationInterface(this.gui, app);
         this.sun = new SunInterface(this.gui, app);
         this.planet = new PlanetInterface(this.gui, app);
         this.spawn = new SpawnInterface(this.gui, app);
@@ -27,6 +29,7 @@ export default class Interface {
     }
 
     hotReplaceApplication(app: Application) {
+        this.simulation.hotReplaceApplication(app);
         this.sun.hotReplaceApplication(app);
         this.camera.hotReplaceApplication(app);
         this.planet.hotReplaceApplication(app);
---
+++ src/interface/simulation-interface.ts
@@ -0,0 +1,28 @@
+import GUI from 'lil-gui';
+import type Application from 'app';
+
+export default class SimulationInterface {
+    protected readonly folder = this.gui.addFolder('Simulation');
+
+    protected resolution = Math.log10(this.app.world.timeResolution);
+    protected timescale = this.app.world.timescale;
+
+    constructor(protected readonly gui: GUI, protected app: Application) {
+        // this.folder.open(false); // closed by default.
+
+        this.folder.add(this, 'resolution').name('Time resolution').min(-8).max(2).step(1);
+        this.folder.add(this, 'timescale').name('Time scale').min(0.1).max(1e4).step(1);
+
+        this.folder.onChange(this.apply.bind(this));
+    }
+
+    hotReplaceApplication(app: Application) {
+        this.app = app;
+        this.apply();
+    }
+
+    protected apply() {
+        this.app.world.timeResolution = Math.pow(10, this.resolution);
+        this.app.world.timescale = this.timescale;
+    }
+}
