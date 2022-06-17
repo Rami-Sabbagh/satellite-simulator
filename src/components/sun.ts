c28dd903293adafb918584711ccee1f8d8bbf2a3
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-17 12:15:11 +0300

    Remove duplicated code

--- src/components/sun.ts
+++ src/components/sun.ts
@@ -19,12 +19,7 @@ export default class Sun extends THREE.Object3D {
 		this.add(this.mesh);
 		this.add(this.light);
 
-		const lensflare = new Lensflare();
-		lensflare.addElement(new LensflareElement(lensflareBaseTexture, 700, 0, this.light.color));
-		lensflare.addElement(new LensflareElement(lensflareExtraTexture, 60, 0.1, this.light.color));
-		lensflare.addElement(new LensflareElement(lensflareExtraTexture, 70, 0.2, this.light.color));
-		lensflare.addElement(new LensflareElement(lensflareExtraTexture, 120, 0.3, this.light.color));
-		this.light.add(lensflare);
+		this.lensflareScaler = 1; // initialize the lensflare.
 
 		this.position.z = distance;
 	}
