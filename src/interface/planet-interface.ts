98e1314d5c52f70caa4e624c9fd54c66671ceec4
Yaman Qassas <yaman102011@gmail.com>
2022-06-18 22:12:23 +0300

    Fix Planet `Period` slider's max value

--- src/interface/planet-interface.ts
+++ src/interface/planet-interface.ts
@@ -26,7 +26,7 @@ export default class PlanetInterface {
     constructor(protected readonly gui: GUI, protected app: Application) {
         this.folder.open(false); // closed by default.
 
-        this.folder.add(this, 'period').name('Period').min(1).max(24 * 10).step(6);
+        this.folder.add(this, 'period').name('Period').min(1).max(this.app.world.planet.period * 2).step(6);
         this.folder.add(this, 'texture', texturesOptions).name('Texture')
             .onChange(() => {
                 this.bumpScale = textures[`${this.texture.charAt(0).toLowerCase()}${this.texture.slice(1)}`].bumpScale ?? 0;
