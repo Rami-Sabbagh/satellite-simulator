ad4981992de4b160922434d99de31fea7e096e51
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-06-20 18:39:44 +0300

    Remove redundant comments from constants.ts
    I think the variable names speak for themselves, there's no need for additional noise in the file.

--- src/physics/constants.ts
+++ src/physics/constants.ts
@@ -2,38 +2,18 @@ export const GRAVITATION_CONSTANT = 6.674e-11;
 
 export const SIMULATION_SCALE = 1;
 
-/**
- * Earth's estimated mass.
- */
 export const EARTH_MASS = 5.972e24;
-/**
- * Earth's estimated radius.
- */
+
 export const EARTH_RADIUS = 6.371e6;
-/**
- * Earth's average distance from Sun.
- */
-export const EARTH_DISTANCE = 1.496e11;
 
-/**
- * Earth's natural moon's estimated mass.
- */
+export const EARTH_DISTANCE = 1.496e11; // Distance between Earth and sun
+
 export const MOON_MASS = 7.34767309e22;
-/**
- * Earth's natural moon's estimated radius.
- */
+
 export const MOON_RADIUS = 1.737e6;
-/**
- * Earth's natural moon's average distance from Earth.
- */
-export const MOON_DISTANCE = 3.844e8;
 
-/**
- * Sun's estimated mass.
- */
+export const MOON_DISTANCE = 3.844e8; // Distance between moon and Earth
+
 export const SUN_MASS = 1.989e30;
-/**
- * Sun's estimated radius.
- */
-export const SUN_RADIUS = 6.955e8;
 
+export const SUN_RADIUS = 6.955e8;
