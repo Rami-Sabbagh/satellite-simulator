dcabd6eef6a9417a5c3eceb97e7d896c18cc04ee
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-22 12:47:41 +0300

    Use JSDocs instead of normal comments

--- src/physics/constants.ts
+++ src/physics/constants.ts
@@ -6,13 +6,19 @@ export const EARTH_MASS = 5.972e24;
 
 export const EARTH_RADIUS = 6.371e6;
 
-export const EARTH_DISTANCE = 1.496e11; // Distance between Earth and sun
+/**
+ * Distance between Earth and Sun.
+ */
+export const EARTH_DISTANCE = 1.496e11;
 
 export const MOON_MASS = 7.34767309e22;
 
 export const MOON_RADIUS = 1.737e6;
 
-export const MOON_DISTANCE = 3.844e8; // Distance between moon and Earth
+/**
+ * Distance between Moon and Earth.
+ */
+export const MOON_DISTANCE = 3.844e8;
 
 export const SUN_MASS = 1.989e30;
 
